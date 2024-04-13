c79fcc4472924c51c10580607819315865c6e967
Anthony Santos <anthony.santos1824@gmail.com>
2024-04-13 01:11:57 -0400

    Need to finish Readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# SVG-logo-maker-
+
+# Project Title: SVG-logo-Generator
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+## Description
+This project uses inquirer to ask  the user some input to create the logo for exmaple the project asks the user input 3 letters, text color , and background color. And from this input the program will create a logo . And we also implemented FS in the program so we are able 
+
+## Table of Contents
+* [Installation](#installation) 
+ 
+* [License](#license) 
+ 
+* [Questions](#questions) 
+
+* [Screenshot](#screenshot) 
+ 
+
+
+## Installation Instructions
+```bash
+To install the project. You can clone the project on Github and then download all the necesarry dependencies and then run the program using 'node index.js'
+```
+
+
+## License
+MIT
+
+## Questions
+​
+<img src="https://avatars.githubusercontent.com/u/82828956?v=4" alt="avatar" style="border-radius: 16px" width="30" />
+​
+If you have any questions about the repo, open an issue or contact [mynameisandreee](https://github.com/mynameisandreee) directly at anthony.santos1824@gmail.com.
+
+## Screenshot
+![alt text](image.png)
+![alt text](image-1.png)
+
+## Video link
+
+
+
--- index.js
+++ index.js
@@ -1,39 +1,75 @@
-const questions = require('inquirer');
+const questions = require("inquirer");
+const fs = require("fs");
+//importing the classes
+const { Triangle, Square, Circle } = require("./lib/shape");
 
-questions
-  .prompt([
-    {
-      type: 'input',
-      message: 'Enter up to three characters',
-      name: 'chars',
-    },
-    {
-      type: 'input',
-      message: 'Enter a color (keyword or hexadecimal number)?',
-      name: 'color',
-    },
-    {
-      type: 'list',
-      message: 'Select a shape?',
-      name: 'shape',
-      choices: [
-        'circle',
-        'triangle',
-        'square'
-      ]
-    },
-    {
-        type: 'input',
-        message: 'Shape color keyword or hexadecimal number)?',
-        name: 'shapeColor',
+function writeFile(fileName, answers) {
+  //empty string to store the file content
+  let fileContent = "";
+  // logo container diameter
+  fileContent = `<svg version ="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+  fileContent += "<g>";
+  fileContent += `${answers.shape}`;
+  // if satements to check the shape and color
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    fileContent += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}" />`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    fileContent += `<rect x="73" y="40" width="170" height="170" fill="${answers.shapeColor}" />`;
+  } else if (answers.shape === "Circle") {
+    shapeChoice = new Circle();
+    fileContent += `<circle cx="150" cy="120" r="80" fill="${answers.shapeColor}" />`;
+  }
+  // the following code will add the text to the logo taken from the user input
+  fileContent += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.chars}</text>`;
+  //closing the svg tag
+  fileContent += "</g>";
+  //closing svg tag
+  fileContent += "</svg>";
+
+  fs.writeFile(fileName, fileContent, (err) => {
+    if (err) throw err;
+    console.log("Generated logo.svg");
+  });
+}
+
+function promptUser() {
+  questions
+    .prompt([
+      {
+        type: "input",
+        message: "Enter up to three characters",
+        name: "chars",
       },
-  ]).then(answers => {
-    var file = JSON.stringify(answers, null, '  ')
-    const fs = require('fs');
-    //TODO: create file named logo.svg and then print 'Generated logo.svg'
-    fs.writeFile('logo.svg', file, (err) => {
-        if (err) throw err;
-        console.log('Generated logo.svg');
-    }
-  );
-});
+      {
+        type: "input",
+        message: "Enter a color for your text (keyword or hexadecimal number)?",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "Select a shape?",
+        choices: ["Circle", "Triangle", "Square"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message: "Shape background color (keyword or hexadecimal number)?",
+        name: "shapeColor",
+      },
+    ])
+   .then((answers) => {
+      //error handler if user put more than 3 characters
+      if (answers.chars.length > 3) {
+        console.log("Only three characters are allowed or less");
+        promptUser();
+        return;
+      } else {
+        writeFile("logo.svg", answers);
+      }
+    });
+}
+//calling the prompt user function
+promptUser();
